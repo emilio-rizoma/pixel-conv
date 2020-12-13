d56c01b80ee374e7546ea0296d217fe69577d9a6
emilioparra <emilio.parra@rizomadev.com>
2020-12-13 12:46:03 -0600

    prepare for release

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,19 @@
 import { HttpModule, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MulterModule } from '@nestjs/platform-express';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SortController } from './sort/sort.controller';
 
 @Module({
-  imports: [HttpModule, MulterModule.register({
-    dest: './public/img',
-  })],
+  imports: [
+    HttpModule,
+    MulterModule.register({
+      dest: './public/img',
+    }),
+    ConfigModule.forRoot(),
+  ],
   controllers: [AppController, SortController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
--- src/sort/sort.controller.ts
+++ src/sort/sort.controller.ts
@@ -1,75 +1,81 @@
-import { Controller, Get, HttpService, Param, Post, Req, Res, UploadedFile, UseInterceptors } from '@nestjs/common';
-import { FileInterceptor, MulterModule } from '@nestjs/platform-express';
-import { createReadStream } from 'fs';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Req,
+  Res,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { editFileName, imageFileFilter } from 'src/utils/file-uploading.utils';
 import { convolute } from 'src/utils/sorter.utils';
 
 export interface RequestFile {
-    /** Field name specified in the form */
-    fieldname: string;
-    /** Name of the file on the user's computer */
-    originalname: string;
-    /** Encoding type of the file */
-    encoding: string;
-    /** Mime type of the file */
-    mimetype: string;
-    /** Size of the file in bytes */
-    size: number;
-    /** The folder to which the file has been saved (DiskStorage) */
-    destination: string;
-    /** The name of the file within the destination (DiskStorage) */
-    filename: string;
-    /** Location of the uploaded file (DiskStorage) */
-    path: string;
-    /** A Buffer of the entire file (MemoryStorage) */
-    buffer: Buffer;
+  /** Field name specified in the form */
+  fieldname: string;
+  /** Name of the file on the user's computer */
+  originalname: string;
+  /** Encoding type of the file */
+  encoding: string;
+  /** Mime type of the file */
+  mimetype: string;
+  /** Size of the file in bytes */
+  size: number;
+  /** The folder to which the file has been saved (DiskStorage) */
+  destination: string;
+  /** The name of the file within the destination (DiskStorage) */
+  filename: string;
+  /** Location of the uploaded file (DiskStorage) */
+  path: string;
+  /** A Buffer of the entire file (MemoryStorage) */
+  buffer: Buffer;
 }
 
 @Controller('sort')
 export class SortController {
-    constructor() { }
+  @Post('upload')
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: './public/img',
+        filename: editFileName,
+      }),
+      fileFilter: imageFileFilter,
+    }),
+  )
+  async uploadedFile(@UploadedFile() file, @Req() req) {
+    const reqFile = file as RequestFile;
+    const baseurl = req.headers.host;
+    const domain = process.env.DOMAIN;
+    const response = {
+      originalname: reqFile.originalname,
+      filename: reqFile.filename,
+      url: `https://{{${baseurl}}}/${domain}/sort/process/${reqFile.filename}`,
+    };
+    return response;
+  }
 
-    @Post('upload')
-    @UseInterceptors(
-        FileInterceptor('image', {
-            storage: diskStorage({
-                destination: './public/img',
-                filename: editFileName,
-            }),
-            fileFilter: imageFileFilter,
-        }),
-    )
-    async uploadedFile(@UploadedFile() file) {
-        const reqFile = file as RequestFile;
-        const response = {
-            originalname: reqFile.originalname,
-            filename: reqFile.filename,
-            url: `http://localhost:3000/sort/process/${reqFile.filename}`
-        };
-        return response;
-    }
+  @Get('process/:imgpath')
+  async processFile(@Param('imgpath') image, @Req() req) {
+    const { kernel, rounds, floyd } = req.query;
+    const name = image.replace('.jpeg', '');
+    const path = `./public/img/${image}`;
+    const conf = await convolute.run(name, path, kernel, rounds, floyd);
+    const baseurl = req.headers.host;
+    const domain = process.env.DOMAIN;
+    const response = {
+      originalfile: image,
+      manipulatedfile: conf,
+      url: `https://${baseurl}/${domain}/sort/download/${conf}`,
+    };
+    return response;
+  }
 
-    @Get('process/:imgpath')
-    async processFile(@Param('imgpath') image, @Req() req) {
-        const { kernel, rounds, floyd } = req.query;
-        const name = image.replace('.jpeg', '');
-        const path = `./public/img/${image}`;
-        const conf = await convolute.run(name, path, kernel, rounds, floyd);
-        const response = {
-            originalfile: image,
-            manipulatedfile: conf,
-            url: `http://localhost:3000/sort/download/${conf}`
-        };
-        return response;
-    }
-
-
-    @Get('download/:imgpath')
-    seeUploadedFile(@Param('imgpath') image, @Res() res) {
-        return res.sendFile(image, { root: './public/output' });
-    }
-
-
-
+  @Get('download/:imgpath')
+  seeUploadedFile(@Param('imgpath') image, @Res() res) {
+    return res.sendFile(image, { root: './public/output' });
+  }
 }
